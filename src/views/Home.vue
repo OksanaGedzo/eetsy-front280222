ceb86f95535e97793d62cd4068b73820a55db87d
Rait Rotsan <rait.r@hotmail.com>
2022-02-15 14:44:28 +0200

    Added test div v-for loop and methods.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="home">
 
-    {{this.primaryGroupObjects.length}}
+<!--    {{this.primaryGroupObjects.length}}-->
+<!--    {{this.primaryGroupObjects}}-->
 
     <div v-for="category in primaryGroupObjects" class="row">
-      <div class="column"></div>
+      <div class="column"> <img @click="toSubGroup(category.id)" src="..\assets\logo.png"> </div>
     </div>
+    
+<!--    <img @click="toSubGroup" src="..\assets\logo.png">-->
 
   </div>
 </template>
@@ -21,7 +24,7 @@ export default {
   },
   data: function () {
     return {
-      primaryGroupObjects: {}
+      primaryGroupObjects: {},
     }
   },
   methods: {
@@ -35,7 +38,21 @@ export default {
           }).catch(error => {
         console.log(error)
       })
+    },
+
+    toSubGroup: function (idCodeParam) {
+      this.$http.get("/about", {
+            params: {
+              id: idCodeParam
+            }
+          }
+      ).then(response => {
+        console.log(this.id)
+      }).catch(error => {
+        console.log(error)
+      })
     }
+
   },
   beforeMount() {
     this.getAllPrimaryGroups()
