730595c8e7ff4e2fbe5718acf42df5ad60aad1f6
Rait Rotsan <rait.r@hotmail.com>
2022-02-18 00:19:24 +0200

    Added userId saving to localfiles and basic Logout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
     <div id="nav">
-
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/signup">Sign Up</router-link>
+      <router-link to="/">Home |</router-link> 
+      <router-link to="/about">About |</router-link>  
+      <router-link v-if="!this.userIsLoggedIn" to="/login">Login |</router-link> 
+      <router-link v-if="!this.userIsLoggedIn" to="/signup">Sign Up |</router-link> 
+      <a href="/" v-if="this.userIsLoggedIn" v-on:click="logUserOut">Log Out</a>
+      {{this.userIsLoggedIn}}
+      <!-- navbari element ei refreshi ennast kui seal sees ei toimu mingit tüüpi event, näiteks nupu vajutus -->
+    
+      <button v-on:click="isUserLoggedIn">Check if user is logged in</button>
+    
     </div>
-    <router-view/>
+    <router-view :key="$route.fullPath"/>
   </div>
 </template>
 
@@ -33,3 +38,33 @@
   color: #42b983;
 }
 </style>
+
+<script>
+export default {
+  name: "App",
+  data: function() {
+    return {
+    userIsLoggedIn: false
+        }
+    },
+    methods: {
+      isUserLoggedIn: function(){
+        if (localStorage.getItem("UserIdToken") === null){
+          this.userIsLoggedIn = false;
+          alert("id is null")
+        }
+        else{
+          this.userIsLoggedIn = true;
+          alert("id exists")
+        }
+      },
+    logUserOut: function(){
+      localStorage.removeItem('UserIdToken');
+      this.userIsLoggedIn = false
+    },
+    beforeMount(){
+            this.isUserLoggedIn;
+    },
+  }
+}
+</script>
