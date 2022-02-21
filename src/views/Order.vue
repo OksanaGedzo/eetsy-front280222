037a5ac72b483201580ab82e52dc1bb539cce55b
OksanaGedzo <invogueme@gmail.com>
2022-02-21 16:59:09 +0200

    shopping cart

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -2,51 +2,69 @@
   <div>
     <img alt="Vue logo" src="../assets/logo.png">
     <h1>Shopping cart order</h1>
-    <div>
-      Order Number: {{ orderNumber }}
-      <br>
-      <div v-for="row in order">
-        OrderItemsByOrderId: {{ row.orderItemsByOrderId }}<br>
-        ItemName: {{ row.itemName }}<br>
-        Picture: {{ row.itemPictureId }}<br>
-        Price: {{ row.itemPrice }}<br>
-        Quantity: {{ row.orderItemQuantity }}<br>
-        Total sum: {{ row.orderItemSum }}<br>
-      </div>
-      <br>
-      <div class="selDiv">
-        <select class="opts">
-          <option selected value="DEFAULT">Vali delivery method</option>
-          <option value="SEL1">Itella</option>
-          <option value="SEL2">Omniva</option>
-          <option value="SEL2">StarShip</option>
-          <option value="SEL2">Kuller</option>
-        </select>
-        <br>
-        <br>
-        <select class="opts">
-          <option selected value="DEFAULT">Vali payment method</option>
-          <option value="SEL1">Bank</option>
-          <option value="SEL2">PayPal</option>
-          <option value="SEL2">Sularaha</option>
-          <option value="SEL2">MetaMask</option>
-        </select>
-        <br>
-        <br>
-        TOTAL PRICE: {{ 'ghh  method  jj' }}
-        <br>
-        <br>
-        <button v-on:click="redirectToConfirmPage(orderId.id)">Confirm the order</button>
-        <br>
-        <br>
-        <br>
-      </div>
-      <br>
-    </div>
+    Order Number: {{ orderNumber }}
+    <!--    <div>-->
+    <!--      Order Number: {{ orderNumber }}-->
+    <!--      <br>-->
+    <!--      <div v-for="row in order">-->
+    <!--        OrderItemsByOrderId: {{ row.orderItemsByOrderId }}<br>-->
+    <!--        ItemName: {{ row.itemName }}<br>-->
+    <!--        Picture: {{ row.itemPictureId }}<br>-->
+    <!--        Price: {{ row.itemPrice }}<br>-->
+    <!--        Quantity: {{ row.orderItemQuantity }}<br>-->
+    <!--        Total sum: {{ row.orderItemSum }}<br>-->
+    <!--      </div>-->
+    <br>
+    <!--      <div class="selDiv">-->
+    <!--        <select class="opts">-->
+    <!--          <option selected value="DEFAULT">Vali delivery method</option>-->
+    <!--          <option value="SEL1">Itella</option>-->
+    <!--          <option value="SEL2">Omniva</option>-->
+    <!--          <option value="SEL2">StarShip</option>-->
+    <!--          <option value="SEL2">Kuller</option>-->
+    <!--        </select>-->
+    <br>
+    <br>
+    <ol>
+      <li v-for=" row in orderItemDtos" > {{row.itemName}} - {{row.itemPrice}} eur- {{row.quantity}} tk - {{row.sum}} eur</li>
+    </ol>
+    <select class="opts">
+      <option selected value="DEFAULT">Vali payment method</option>
+      <option v-for="row in paymentMethods" :value="row.id">{{ row.paymentType }}</option>
+    </select>
+    <select class="opts">
+      <option selected value="DEFAULT">Vali delivery method</option>
+      <option v-for="row in deliveryMethods" :value="row.id">{{ row.name }}</option>
+    </select>
+    <!--        TOTAL PRICE: {{ 'ghh  method  jj' }}-->
+    <!--        <button v-on:click="redirectToConfirmPage(orderId.id)">Confirm the order</button>-->
+    <!--      </div>
+        </div>-->
   </div>
 </template>
 
 
+<style>
+table {
+  margin-left: auto;
+  margin-right: auto;
+}
+
+td {
+  padding: 7px;
+  border-bottom: solid;
+  border-width: 1px;
+  border-color: lightgray;
+}
+
+th {
+  padding: 10px;
+  background-color: lightgray;
+  border: solid;
+  border-width: 1px;
+  border-color: lightgray;
+}
+</style>
 <script>
 export default {
   name: "Order",
@@ -54,12 +72,15 @@ export default {
     return {
       orderId: '',
       orderNumber: '',
-      userId: '',
+      userId: sessionStorage.getItem("UserIdToken"),
       orderItemsByOrderId: this.$route.query.orderItemId,
       order: {},
       deliveryMethods: '',
       orderItemSum: '',
-      paymentMethods: ''
+
+      orderItemDtos: [],
+      paymentMethods: [],
+      deliveryMethods: [],
 
     }
   },
@@ -104,10 +125,7 @@ export default {
 
 
   beforeMount() {
-    this.getOrderItemsByOrderId(this.orderItemsByOrderId)
-    this.getDeliveryMethods(this.deliveryMethods)
-    this.getPaymentMethods(this.paymentMethods)
-    this.getOrderNumber(this.orderNumber)
+    this.getShoppingCart()
   }
 }
 </script>
