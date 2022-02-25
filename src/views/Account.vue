18ae8761fdbfd25c6c71e023480947e7a1d36b35
merikese <merekivi@gmail.com>
2022-02-25 13:27:09 +0200

    added update contact info

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -9,14 +9,39 @@
           <th></th>
           <th></th>
         </tr>
-        <tr><td>First Name</td><td><input value="firstName" v-model="firstName"></td></tr>
-        <tr><td>Last Name</td><td><input value="lastName" v-model="lastName"></td></tr>
-        <tr><td>Address</td><td><input value="address" v-model="address"></td></tr>
-        <tr><td>Zip code</td><td><input value="postalIndex" v-model="postalIndex"></td></tr>
-        <tr><td>Country</td><td><input value="country" v-model="country"></td></tr>
-        <tr><td>☎️</td><td><input value="phoneNumber" v-model="phoneNumber"></td></tr>
-        <tr><td>📧</td><td><input value="email" v-model="email"></td></tr>
-
+        <tr>
+          <td>First Name</td>
+          <td><input value="firstName" v-model="firstName"></td>
+        </tr>
+        <tr>
+          <td>Last Name</td>
+          <td><input value="lastName" v-model="lastName"></td>
+        </tr>
+        <tr>
+          <td>Address</td>
+          <td><input value="address" v-model="address"></td>
+        </tr>
+        <tr>
+          <td>Zip code</td>
+          <td><input value="postalIndex" v-model="postalIndex"></td>
+        </tr>
+        <tr>
+          <td>Country</td>
+          <td><input value="country" v-model="country"></td>
+        </tr>
+        <tr>
+          <td>☎️</td>
+          <td><input value="phoneNumber" v-model="phoneNumber"></td>
+        </tr>
+        <tr>
+          <td>📧</td>
+          <td><input value="email" v-model="email"></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><button v-on:click="updateUserInfo">Update account</button></td>
+        </tr>
+        <br>
 
       </table>
     </div>
@@ -29,15 +54,14 @@ export default {
   data: function () {
     return {
       userId: sessionStorage.getItem("UserIdToken"),
-      user:null,
-      firstName:null,
-      lastName:null,
-      address:null,
-      postalIndex:null,
-      country:null,
-      phoneNumber:null,
-      email:null
-
+      user: null,
+      firstName: null,
+      lastName: null,
+      address: null,
+      postalIndex: null,
+      country: null,
+      phoneNumber: null,
+      email: null,
     }
   },
   methods: {
@@ -46,9 +70,9 @@ export default {
       this.lastName = this.user.contact.lastName
       this.address = this.user.contact.address
       this.postalIndex = this.user.contact.postIndex
-      this.country= this.user.contact.country
-      this.phoneNumber= this.user.contact.phoneNumber
-      this.email= this.user.contact.email
+      this.country = this.user.contact.country
+      this.phoneNumber = this.user.contact.phoneNumber
+      this.email = this.user.contact.email
 
     },
     isUserLoggedIn: function () {
@@ -56,8 +80,6 @@ export default {
         alert("Please log in")
       } else {
         this.getUserInfo()
-
-
       }
     },
     getUserInfo: function () {
@@ -73,8 +95,30 @@ export default {
       }).catch(error => {
         console.log(error)
       })
-    }
-
+    },
+    updateUserInfo: function () {
+      let contactObject= {
+        "firstName": this.firstName,
+            "lastName": this.lastName,
+            "address": this.address,
+            "postIndex": this.postIndex,
+            "country": this.country,
+            "phoneNumber": this.phoneNumber,
+            "email": this.email,
+            "id":this.id
+      }
+      this.$http.put("/change/user/contact/info", contactObject
+      ).then(response => {
+        alert("Contact information is updated")
+        this.redirectToHomePage()
+        console.log(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+    redirectToHomePage: function () {
+      this.$router.push({name: 'Home'})
+    },
   },
   beforeMount() {
     this.isUserLoggedIn()
